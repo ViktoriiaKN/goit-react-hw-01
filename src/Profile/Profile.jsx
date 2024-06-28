45e46a13cbd332294acd24f16d7071f88cbe0088
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-06-28 21:46:51 +0200

    Styles for Profie

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,30 +1,31 @@
+import s from './Profile.module.css';
 
-const Profile = ({name, tag, location, image, stats}) => {
+const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
-  <div>
-    <img src={image} alt={`${name}'s avatar`}/>
-    <p>{name}</p>
-    <p>@{tag}</p>
-    <p>{location}</p>
-  </div>
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={image} alt={`${name}'s avatar`} className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
 
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>{stats.followers}</span>
-    </li>
-    <li>
-      <span>Views</span>
-      <span>{stats.views}</span>
-    </li>
-    <li>
-      <span>Likes</span>
-      <span>{stats.likes}</span>
-    </li>
-  </ul>
-</div>
-  )
+      <ul className={s.stats}>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Profile
