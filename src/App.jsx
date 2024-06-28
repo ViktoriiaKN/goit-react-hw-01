ef5d3edd7d9af2d7db8d00eada97a895a1fa6035
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-06-28 21:27:33 +0200

    Tasks 2, 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './App.css';
 import Profile from './Profile/Profile';
 import userData from './userData.json';
+import friends from './friends.json';
+import FriendList from './FriendList/FriendList';
+import transactions from './transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 function App() {
 
   return (
     <>
-     <Profile
+    <Profile
      name={userData.username}
      tag={userData.tag}
      location={userData.location}
      image={userData.avatar}
      stats={userData.stats}
-     />
+     /> 
+     <FriendList friends={friends}/>
+     <TransactionHistory items={transactions}/>
     </>
   )
 }
